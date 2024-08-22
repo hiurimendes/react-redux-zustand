14087b12a7c8591f65daedf70b1f4154b2b1aef1
Hiuri Mendes <hiurimendes@gmail.com>
2024-08-22 16:28:45 -0300

    Criando hook global

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,8 @@
 import { MessageCircle } from "lucide-react";
-import { useAppSelector } from "../store";
+import { useCurrentLesson } from "../store/slices/player";
 
 export function Header() {
-  const { currentModule, currentLesson } = useAppSelector(state => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentModule = state.player.course.modules[currentModuleIndex]
-    const currentLesson = currentModule.lessons[currentLessonIndex]
-
-    return { currentModule, currentLesson }
-  })
+  const { currentModule, currentLesson } = useCurrentLesson()
 
   return (
     <div className="flex items-center justify-between">
